017445ea42c45488417b49312e30b493b47c5127
sunwise <sunwise9634@gmail.com>
2025-09-21 15:48:11 +0900

    method study

---
+++ src/method/Method1.java
@@ -0,0 +1,20 @@
+package method;
+
+public class Method1 {
+
+    public static void main(String[] args) {
+        // 계산1
+        int a = 1;
+        int b = 2;
+        System.out.println(a + b);
+        int sum1 = a + b;
+        System.out.println("결과 출력: " + sum1);
+
+        // 계산 2
+        int x = 10;
+        int y = 20;
+        System.out.println(x + "+"+ y);
+        int sum2 = x + y;
+        System.out.println(sum2);
+    }
+}
